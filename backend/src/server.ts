65259b45561c6c3387871fcb4efceadac38ba9a0
SahilNambiar11 <sahil.nambiar11@gmail.com>
2026-01-05 13:06:38 -0500

    fixed port handling again

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,11 @@
 import app from "./app";
+import 'dotenv/config';
+
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
 const PORT = process.env.PORT || 8081;
+
+console.log('process.env.PORT type:', typeof process.env.PORT, 'value:', process.env.PORT);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
